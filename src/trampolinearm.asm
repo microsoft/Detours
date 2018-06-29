1e49d3c1e978a63e5a757095e75160146a2f23c7
unknown v2 <unknownv2@users.noreply.github.com>
2018-06-29 00:32:39 -0400

    Start work on ARM32  trampoline ASM

---
+++ src/trampolinearm.asm
@@ -0,0 +1,15 @@
+   AREA     .text, CODE, THUMB
+                           
+Trampoline_ASM_ARM FUNCTION
+
+        EXPORT  Trampoline_ASM_ARM 
+         
+start                           
+        MOV      r0, #10
+        MOV      r1, #3
+        ADD      r0, r0, r1
+stop
+        MOV      r0, #0x18
+
+        ENDFUNC
+        END                     
