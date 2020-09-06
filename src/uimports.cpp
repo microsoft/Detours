107d601855fdf0258f47bdce2b863b549a0393de
sonyps5201314 <sonyps5201314@gmail.com>
2020-09-06 19:50:03 +0800

    The code format is adapted to the Detours code style, and more security checks are performed.

--- src/uimports.cpp
+++ src/uimports.cpp
@@ -106,33 +106,35 @@ static BOOL UPDATE_IMPORTS_XX(HANDLE hProcess,
         }
     }
 
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Added code
-	if (inh.IMPORT_DIRECTORY.VirtualAddress!=NULL && inh.IMPORT_DIRECTORY.Size==0)
-	{
-		//Don't worry about changing the PE file, because the load information of the original PE header has been saved and will be restored. The change here is just for the following code to work normally
-		PIMAGE_IMPORT_DESCRIPTOR pImageImport=(PIMAGE_IMPORT_DESCRIPTOR)(pbModule+inh.IMPORT_DIRECTORY.VirtualAddress);
-		IMAGE_IMPORT_DESCRIPTOR ImageImport;
+    if (inh.IMPORT_DIRECTORY.VirtualAddress != NULL && inh.IMPORT_DIRECTORY.Size == 0) {
+        
+        //Don't worry about changing the PE file, 
+        //because the load information of the original PE header has been saved and will be restored. 
+        //The change here is just for the following code to work normally
+        
+        PIMAGE_IMPORT_DESCRIPTOR pImageImport = (PIMAGE_IMPORT_DESCRIPTOR)(pbModule + inh.IMPORT_DIRECTORY.VirtualAddress);
+        IMAGE_IMPORT_DESCRIPTOR ImageImport;
 
-		if(!ReadProcessMemory(hProcess,pImageImport,&ImageImport,sizeof(ImageImport),NULL))
-		{
-			_Analysis_assume_(FALSE);
-			goto finish;
-		}
-		while(ImageImport.Name)
-		{
-			inh.IMPORT_DIRECTORY.Size+=sizeof(IMAGE_IMPORT_DESCRIPTOR);
-			if(!ReadProcessMemory(hProcess,pImageImport,&ImageImport,sizeof(ImageImport),NULL))
-			{
-				_Analysis_assume_(FALSE);
-				goto finish;
-			}
-			++pImageImport;
-		}
-		OutputDebugString(TEXT("[This PE file has an import table, but the import table size is marked as 0. This is an error.")
-			TEXT("Not repair will cause the launched program to not work normally, and its import table size has been automatically repaired for you! ! !]\r\n"));
-	}
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        if (!ReadProcessMemory(hProcess, pImageImport, &ImageImport, sizeof(ImageImport), NULL)) {
+            _Analysis_assume_(FALSE);
+            goto finish;
+        }
+        while (ImageImport.Name) {
+            inh.IMPORT_DIRECTORY.Size += sizeof(IMAGE_IMPORT_DESCRIPTOR);
+            if (!ReadProcessMemory(hProcess, pImageImport, &ImageImport, sizeof(ImageImport), NULL)) {
+                _Analysis_assume_(FALSE);
+                goto finish;
+            }
+            ++pImageImport;
+        }
+
+        DWORD dwLastError = GetLastError();
+        OutputDebugString(TEXT("[This PE file has an import table, but the import table size is marked as 0. This is an error.")
+            TEXT("If it is not repaired, the launched program will not work properly, Detours has automatically repaired its import table size for you! ! !]\r\n"));
+        if (GetLastError() != dwLastError) {
+            SetLastError(dwLastError);
+        }
+    }
 
     DETOUR_TRACE(("     Imports: %p..%p\n",
                   (DWORD_PTR)pbModule + inh.IMPORT_DIRECTORY.VirtualAddress,
