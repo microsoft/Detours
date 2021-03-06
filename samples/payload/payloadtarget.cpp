ecf886d015aae1dc0a57db9acaed8b0333403558
Charles Milette <charles.milette@gmail.com>
2021-03-05 20:45:01 -0500

    Remove tabs in the payload sample

--- samples/payload/payloadtarget.cpp
+++ samples/payload/payloadtarget.cpp
@@ -11,50 +11,50 @@ HANDLE hParent = NULL;
 
 __declspec(noreturn) void HandleApiFailure(const char* api)
 {
-	DWORD lastErr = GetLastError();
-	std::cout << "payloadtarget.exe: " << api << " failed (" << lastErr << ')' << std::endl;
+    DWORD lastErr = GetLastError();
+    std::cout << "payloadtarget.exe: " << api << " failed (" << lastErr << ')' << std::endl;
 
-	if (hParent)
-	{
-		CloseHandle(hParent);
-	}
+    if (hParent)
+    {
+        CloseHandle(hParent);
+    }
 
-	ExitProcess(1);
+    ExitProcess(1);
 }
 
 int main()
 {
-	DWORD payloadSize;
-	void* payloadAddr = DetourFindPayloadEx(PARENT_HANDLE_PAYLOAD, &payloadSize);
-	if (!payloadAddr || payloadSize != sizeof(HANDLE))
-	{
-		HandleApiFailure("DetourFindPayloadEx");
-	}
+    DWORD payloadSize;
+    void* payloadAddr = DetourFindPayloadEx(PARENT_HANDLE_PAYLOAD, &payloadSize);
+    if (!payloadAddr || payloadSize != sizeof(HANDLE))
+    {
+        HandleApiFailure("DetourFindPayloadEx");
+    }
 
-	hParent = *static_cast<HANDLE*>(payloadAddr);
+    hParent = *static_cast<HANDLE*>(payloadAddr);
 
-	DWORD randomPayloadSize;
-	void* randomPayload = DetourFindRemotePayload(hParent, RANDOM_DATA_PAYLOAD, &randomPayloadSize);
-	if (!randomPayload || randomPayloadSize != sizeof(random_payload_t))
-	{
-		HandleApiFailure("DetourFindRemotePayload");
-	}
+    DWORD randomPayloadSize;
+    void* randomPayload = DetourFindRemotePayload(hParent, RANDOM_DATA_PAYLOAD, &randomPayloadSize);
+    if (!randomPayload || randomPayloadSize != sizeof(random_payload_t))
+    {
+        HandleApiFailure("DetourFindRemotePayload");
+    }
 
-	random_payload_t randomData;
-	if (rand_s(&randomData) != 0)
-	{
-		HandleApiFailure("rand_s");
-	}
+    random_payload_t randomData;
+    if (rand_s(&randomData) != 0)
+    {
+        HandleApiFailure("rand_s");
+    }
 
 
-	if (!WriteProcessMemory(hParent, randomPayload, &randomData, sizeof(randomData), NULL))
-	{
-		HandleApiFailure("WriteProcessMemory");
-	}
+    if (!WriteProcessMemory(hParent, randomPayload, &randomData, sizeof(randomData), NULL))
+    {
+        HandleApiFailure("WriteProcessMemory");
+    }
 
-	CloseHandle(hParent);
-	hParent = NULL;
+    CloseHandle(hParent);
+    hParent = NULL;
 
-	// conversion to int return type is potentially undefined
-	ExitProcess(randomData);
+    // conversion to int return type is potentially undefined
+    ExitProcess(randomData);
 }
