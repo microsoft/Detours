b9ffe553d3ea7c01a814d23d088fbfb30b5f4426
jdp1024 <ilford@gmail.com>
2021-04-01 08:21:43 +0800

    Removed some unnecessary static modifiers.

--- src/disasm.cpp
+++ src/disasm.cpp
@@ -664,7 +664,7 @@ PBYTE CDetourDis::CopyF6(REFCOPYENTRY pEntry, PBYTE pbDst, PBYTE pbSrc)
     // NEG /3
     // NOT /2
 
-    static const COPYENTRY ce = /* f6 */ s_rceCopyMap[eENTRY_CopyBytes2Mod];
+    const COPYENTRY ce = /* f6 */ s_rceCopyMap[eENTRY_CopyBytes2Mod];
     return (this->*ce.pfCopy)(&ce, pbDst, pbSrc);
 }
 
@@ -674,7 +674,7 @@ PBYTE CDetourDis::CopyF7(REFCOPYENTRY pEntry, PBYTE pbDst, PBYTE pbSrc)
 
     // TEST WORD /0
     if (0x00 == (0x38 & pbSrc[1])) {    // reg(bits 543) of ModR/M == 0
-        static const COPYENTRY ce = /* f7 */ s_rceCopyMap[eENTRY_CopyBytes2ModOperand];
+        const COPYENTRY ce = /* f7 */ s_rceCopyMap[eENTRY_CopyBytes2ModOperand];
         return (this->*ce.pfCopy)(&ce, pbDst, pbSrc);
     }
 
@@ -684,7 +684,7 @@ PBYTE CDetourDis::CopyF7(REFCOPYENTRY pEntry, PBYTE pbDst, PBYTE pbSrc)
     // MUL /4
     // NEG /3
     // NOT /2
-    static const COPYENTRY ce = /* f7 */ s_rceCopyMap[eENTRY_CopyBytes2Mod];
+    const COPYENTRY ce = /* f7 */ s_rceCopyMap[eENTRY_CopyBytes2Mod];
     return (this->*ce.pfCopy)(&ce, pbDst, pbSrc);
 }
 
@@ -699,7 +699,7 @@ PBYTE CDetourDis::CopyFF(REFCOPYENTRY pEntry, PBYTE pbDst, PBYTE pbSrc)
     // invalid/7
     (void)pEntry;
 
-    static const COPYENTRY ce = /* ff */ s_rceCopyMap[eENTRY_CopyBytes2Mod];
+    const COPYENTRY ce = /* ff */ s_rceCopyMap[eENTRY_CopyBytes2Mod];
     PBYTE pbOut = (this->*ce.pfCopy)(&ce, pbDst, pbSrc);
 
     BYTE const b1 = pbSrc[1];
@@ -845,7 +845,7 @@ PBYTE CDetourDis::CopyEvex(REFCOPYENTRY, PBYTE pbDst, PBYTE pbSrc)
     BYTE const p0 = pbSrc[1];
 
 #ifdef DETOURS_X86
-    static BYTE ceBound = /* 62 */ eENTRY_CopyBytes2Mod;
+    const BYTE ceBound = /* 62 */ eENTRY_CopyBytes2Mod;
     if ((p0 & 0xC0) != 0xC0) {
         return CopyBytes(&s_rceCopyMap[ceBound], pbDst, pbSrc);
     }
