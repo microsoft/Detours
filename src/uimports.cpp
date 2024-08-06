60517d2143a39602ba3f532150c6181459867f1b
Riley Shaw <rishaw@microsoft.com>
2024-08-06 11:33:30 -0700

    add back checksum zeroing

--- src/uimports.cpp
+++ src/uimports.cpp
@@ -306,6 +306,8 @@ static BOOL UPDATE_IMPORTS_XX(HANDLE hProcess,
         goto finish;
     }
 
+    inh.OptionalHeader.CheckSum = 0;
+
     if (!WriteProcessMemory(hProcess, pbModule, &idh, sizeof(idh), NULL)) {
         DETOUR_TRACE(("WriteProcessMemory(idh) failed: %lu\n", GetLastError()));
         goto finish;
